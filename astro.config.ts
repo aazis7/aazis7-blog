cc351aecc018d0ff87eb08b2a52f0fb34519eb5e
aazis7 <azis190407@gmail.com>
2025-07-16 12:13:00 +0000

    chore(build): remove cloudflare adapter from astro config

--- astro.config.ts
+++ astro.config.ts
@@ -11,8 +11,6 @@ import {
 import { transformerFileName } from "./src/utils/transformers/fileName";
 import { SITE } from "./src/config";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
@@ -68,6 +66,4 @@ export default defineConfig({
   experimental: {
     preserveScriptOrder: true,
   },
-
-  adapter: cloudflare(),
 });
