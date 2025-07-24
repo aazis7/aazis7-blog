d8110c7fa86d2431748fb511381819db642cb79c
aazis7 <azis190407@gmail.com>
2025-07-24 12:33:12 +0000

    chore(config): adjust site url config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const SITE = {
-  website: "https://aazis7.pages.dev/", // replace this with your deployed domain
+  website: "https://aazis7-blog.pages.dev/", // replace this with your deployed domain
   author: "Abdul Azis",
   profile: "https://github.com/aazis7",
   desc: "Hai, aku Abdul Azis! Aku suka ngoprek web, udah setahun lebih bikin aplikasi web yang asik dan gampang dipake, pake teknologi kayak [TypeScript](), [React](https://react.dev), [Next.js](), [Express](), sama [Hono](). Selain nulis kode, aku juga nyempetin buat nuangin isi hati dan pikiran melalui cerita pendek, novel, atau puisi yang bikin orang bilang why (wkwk). Di medsos, aku suka share konten seru yang nyampur teknis sama kreatif biar bisa nginspirasi temen-temen semua.",
